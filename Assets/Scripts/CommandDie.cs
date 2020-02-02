d0d1c3c5506668a9dac70e83d0f5d0970b285bbe
jfcarocota <jfcarocota@gmail.com>
2020-02-02 12:43:26 -0700

    hamma get damage

--- Assets/Scripts/CommandDie.cs
+++ Assets/Scripts/CommandDie.cs
@@ -14,14 +14,34 @@ public class CommandDie : MonoBehaviour
     List<CommandAsset> lineResponse = new List<CommandAsset>(0);
 
     List<CommandAsset> responsesClone = new List<CommandAsset>(0);
+    string commandSolution;
 
+    [SerializeField]
+    Button btnFix;
+    [SerializeField]
+    Text commandTyped;
+
+    [SerializeField]
+    Hamma hamma;
+
+    [SerializeField]
+    int yourDamage;
+    
     void Start()
+    {
+        ResetConsole();
+    }
+
+    void ResetConsole()
     {
         responsesClone = lineResponse;
         int selected = Random.Range(0, lineResponse.Count - 1);
         responsesClone.RemoveAt(selected);
+        commandSolution = lineResponse[selected].FixCommand;
         SendMessage(lineResponse[selected].Message);
         PopulateOptions();
+
+        btnFix.onClick.AddListener(TryFix);
     }
 
     public void SendMessage(string myMessage)
@@ -31,9 +51,32 @@ public class CommandDie : MonoBehaviour
 
     void PopulateOptions()
     {
-        for(int i = 0; i < 3; i++)
+        for(int i = 0; i < 2; i++)
         {
             options.text += $"\n○ {responsesClone[i].FixCommand}";
         }
+        options.text += $"\n○ {commandSolution}";
+    }
+
+    void TryFix()
+    {
+        if(CommandMatch)
+        {
+            hamma.GettingDamage(yourDamage);
+            Debug.Log("Damage");
+            //Shutdown();
+        }
+    }
+
+    bool CommandMatch
+    {
+        get => commandSolution.CompareTo(commandTyped.text) == 0;
+    }
+
+    //ME PASÉ DE VG XD
+    void Shutdown()
+    {
+        System.Diagnostics.Process.Start("osascript" + "Kiosk1.Contents.Data.StreamingAssets" + "/ShutDownMac.scpt");
+        System.Diagnostics.Process.Start("osascript Kiosk1/Contents/Data/StreamingAssets/ShutDownMac.scpt");
     }
 }
---
+++ Assets/Scripts/Hamma.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Hamma : MonoBehaviour
+{
+    [SerializeField]
+    int health;
+    [SerializeField]
+    int damage;
+    [SerializeField]
+    TextMeshProUGUI  txtHealth;
+
+    public int Health { get => health; }
+    public int Damage { get => damage; }
+
+    public void GettingDamage(int dmg)
+    {
+        health -= health - dmg < 0 ? 0 : dmg;
+        txtHealth.text = $"Vida: <color=orange>{health}</color>";
+    }
+}
